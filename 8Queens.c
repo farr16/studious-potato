1bee7a950df6ae4bd92c9b04a02b688461fe9ee8
Matthew Farr <msamfarr@outlook.com>
2016-05-10 23:50:29 -0700

    Filled in code for printing the board, with test code for making sure clear was really filling in the board, and to make sure accessing spaces of the board worked as expected

--- 8Queens.c
+++ 8Queens.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE	8 /*length of each side of the board*/
+
 void printBoard(char *board, int len);
 
 /* Program for solving the 8 queens puzzle.
@@ -12,16 +14,26 @@ int main() {
 	int len, x, y;
 	char *board;
 	
+	len = SIZE;
+	
 	/* Allocate space for the board from memory */
 	board = (char *)malloc(len * len * sizeof(char));
+	if (board == NULL) {
+		printf("Memory Allocation ERROR");
+		return 1;
+	}
 	
 	/* Clear the board of any junk data */
 	for(x=0; x<len; x++) {
 		for(y=0; y<len; y++) {
-			*(board + x * len + y ) = ' ';
+			/* Will be blanks in final program, testing to make sure these are 
+			 * really getting filled in by this section of code
+			 */
+			*(board + x * len + y ) = 33 + x*len+y;
 		}
 	}
 	
+	printBoard(board, len);
 }
 
 void printBoard(char * board, int len) {
@@ -29,10 +41,21 @@ void printBoard(char * board, int len) {
 	
 	for(x=0; x<len; x++) {
 		/* Draw top line of row */
+		for(y=0; y<len; y++) {
+			printf("+---");
+		}
+		printf("+\n");
 		/* Draw row of characters */
-		;
+		for(y=0; y<len; y++) {
+			printf("| %c ", *(board + x * len + y));
+		}
+		printf("|\n");
+			
 	}
 	/* Draw the bottom line of the board */
+	for(y=0; y<len; y++)
+		printf("+---");
+	printf("+\n");
 	
 	return;
 }
